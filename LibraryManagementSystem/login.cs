d62e13d123816d19d7cdd4571547eda5f9b7480c
mikosavolainen <miko.markus2112@gmail.com>
2024-06-06 10:06:02 +0300

    login page

--- LibraryManagementSystem/login.cs
+++ LibraryManagementSystem/login.cs
@@ -1,8 +1,8 @@
 namespace LibraryManagementSystem
 {
-    public partial class Form1 : Form
+    public partial class login : Form
     {
-        public Form1()
+        public login()
         {
             InitializeComponent();
         }
