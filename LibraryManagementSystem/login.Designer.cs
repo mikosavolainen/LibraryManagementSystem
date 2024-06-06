442e4e29d80c22521c5f032f7b933682f5ca9e43
mikosavolainen <miko.markus2112@gmail.com>
2024-06-06 12:35:37 +0300

    backup

--- LibraryManagementSystem/login.Designer.cs
+++ LibraryManagementSystem/login.Designer.cs
@@ -1,14 +1,18 @@
-﻿namespace LibraryManagementSystem
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace LibraryManagementSystem
 {
     partial class login
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,36 +27,98 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            groupBox1 = new GroupBox();
             button1 = new Button();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(45, 51);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(45, 125);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Location = new Point(249, 77);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(200, 236);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Login";
             // 
             // button1
             // 
-            button1.Location = new Point(350, 199);
+            button1.BackColor = Color.LightGreen;
+            button1.Location = new Point(63, 178);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "Hieno nappi";
-            button1.UseVisualStyleBackColor = true;
+            button1.TabIndex = 4;
+            button1.Text = "login";
+            button1.UseVisualStyleBackColor = false;
             // 
-            // Form1
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(63, 97);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 3;
+            label2.Text = "password";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(63, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 15);
+            label1.TabIndex = 2;
+            label1.Text = "username";
+            // 
+            // login
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Lavender;
             ClientSize = new Size(800, 450);
-            Controls.Add(button1);
-            Name = "Form1";
-            Text = "Form1";
+            Controls.Add(groupBox1);
+            Name = "login";
+            Text = "Form2";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private GroupBox groupBox1;
+        private Label label2;
+        private Label label1;
         private Button button1;
     }
 }
+
