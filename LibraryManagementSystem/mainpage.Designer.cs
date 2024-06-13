7a7aaf694aa52689769011978acabadf1aa733af
Your Name <you@example.com>
2024-06-13 12:51:56 +0300

    öööööööööö saan kirjoja

--- LibraryManagementSystem/mainpage.Designer.cs
+++ LibraryManagementSystem/mainpage.Designer.cs
@@ -29,17 +29,9 @@
         private void InitializeComponent()
         {
             listBox1 = new ListBox();
-            comboBox1 = new ComboBox();
             textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
             label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
             button1 = new Button();
-            label5 = new Label();
-            button2 = new Button();
             SuspendLayout();
             // 
             // listBox1
@@ -51,14 +43,6 @@
             listBox1.TabIndex = 0;
             listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
-            // comboBox1
-            // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(25, 293);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(151, 28);
-            comboBox1.TabIndex = 1;
-            // 
             // textBox1
             // 
             textBox1.Location = new Point(25, 112);
@@ -67,55 +51,15 @@
             textBox1.TabIndex = 2;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(25, 170);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(151, 27);
-            textBox2.TabIndex = 3;
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(25, 229);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(151, 27);
-            textBox3.TabIndex = 4;
-            // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Location = new Point(25, 79);
             label1.Name = "label1";
-            label1.Size = new Size(38, 20);
+            label1.Size = new Size(53, 20);
             label1.TabIndex = 5;
-            label1.Text = "Title";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(25, 147);
-            label2.Name = "label2";
-            label2.Size = new Size(54, 20);
-            label2.TabIndex = 6;
-            label2.Text = "Author";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(25, 206);
-            label3.Name = "label3";
-            label3.Size = new Size(69, 20);
-            label3.TabIndex = 7;
-            label3.Text = "Publisher";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(25, 270);
-            label4.Name = "label4";
-            label4.Size = new Size(69, 20);
-            label4.TabIndex = 8;
-            label4.Text = "Category";
+            label1.Text = "Search";
+            label1.Click += label1_Click;
             // 
             // button1
             // 
@@ -131,41 +75,14 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(191, 115);
-            label5.Name = "label5";
-            label5.Size = new Size(335, 20);
-            label5.TabIndex = 10;
-            label5.Text = "Title | Author | pubilshing year|publisher|category";
-            label5.Click += label5_Click;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(869, 115);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 29);
-            button2.TabIndex = 11;
-            button2.Text = "More info";
-            button2.UseVisualStyleBackColor = true;
-            // 
             // mainpage
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(975, 628);
-            Controls.Add(button2);
-            Controls.Add(label5);
             Controls.Add(button1);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
             Controls.Add(textBox1);
-            Controls.Add(comboBox1);
             Controls.Add(listBox1);
             Name = "mainpage";
             Text = "Form1";
@@ -177,16 +94,8 @@
         #endregion
 
         private ListBox listBox1;
-        private ComboBox comboBox1;
         private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
         private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
         private Button button1;
-        private Label label5;
-        private Button button2;
     }
 }
--- LibraryManagementSystem/mainpage.cs
+++ LibraryManagementSystem/mainpage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Login.@public;
 
 namespace LibraryManagementSystem
 {
@@ -15,8 +18,43 @@ namespace LibraryManagementSystem
         public mainpage()
         {
             InitializeComponent();
+            getbook();
         }
+        private void getbook()
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(publix.Connect))
+                {
+                    string query = "SELECT * FROM books";
+                    var command = new MySqlCommand(query, connection);
+                    connection.Open();
 
+                    MySqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read()) 
+                    {
+                        var Title = reader["Title"].ToString();
+                        var Author = reader["Author"].ToString();
+                        var Publisher = reader["Publisher"].ToString();
+                        var PublicationYear = reader["PublicationYear"].ToString();
+                        var bookText = Title + " | " + Author + " | " + PublicationYear + " | " + Publisher;
+
+
+                        listBox1.Items.Add(bookText);
+
+                    }
+
+
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
         private void mainpage_Load(object sender, EventArgs e)
         {
 
@@ -39,7 +77,14 @@ namespace LibraryManagementSystem
 
         private void button1_Click(object sender, EventArgs e)
         {
+            var log = new Profile();
+            log.Show();
+            this.Hide();
+        }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("dont click me");
         }
     }
 }
