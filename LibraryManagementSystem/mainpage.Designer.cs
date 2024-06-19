d092ea516ca8329ccb59c6055791cc4ccb6e3fe0
Your Name <you@example.com>
2024-06-19 13:25:09 +0300

    max loan book isv5

--- LibraryManagementSystem/mainpage.Designer.cs
+++ LibraryManagementSystem/mainpage.Designer.cs
@@ -34,34 +34,32 @@
             button1 = new Button();
             buttonSearch = new Button();
             buttonLoan = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(159, 84);
-            listBox1.Margin = new Padding(3, 2, 3, 2);
+            listBox1.Location = new Point(182, 112);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(684, 379);
+            listBox1.Size = new Size(781, 504);
             listBox1.TabIndex = 0;
             listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(22, 84);
-            textBox1.Margin = new Padding(3, 2, 3, 2);
+            textBox1.Location = new Point(25, 112);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(133, 23);
+            textBox1.Size = new Size(151, 27);
             textBox1.TabIndex = 2;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(22, 59);
+            label1.Location = new Point(25, 79);
             label1.Name = "label1";
-            label1.Size = new Size(42, 15);
+            label1.Size = new Size(53, 20);
             label1.TabIndex = 5;
             label1.Text = "Search";
             // 
@@ -69,12 +67,10 @@
             // 
             button1.AllowDrop = true;
             button1.BackgroundImageLayout = ImageLayout.None;
-            button1.Cursor = Cursors.No;
             button1.FlatStyle = FlatStyle.Popup;
-            button1.Location = new Point(746, 16);
-            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Location = new Point(853, 21);
             button1.Name = "button1";
-            button1.Size = new Size(82, 58);
+            button1.Size = new Size(94, 77);
             button1.TabIndex = 9;
             button1.Text = "Profile";
             button1.UseVisualStyleBackColor = true;
@@ -82,36 +78,43 @@
             // 
             // buttonSearch
             // 
-            buttonSearch.Location = new Point(22, 109);
-            buttonSearch.Margin = new Padding(3, 2, 3, 2);
+            buttonSearch.Location = new Point(25, 145);
             buttonSearch.Name = "buttonSearch";
-            buttonSearch.Size = new Size(82, 22);
+            buttonSearch.Size = new Size(94, 29);
             buttonSearch.TabIndex = 10;
             buttonSearch.Text = "Search";
             buttonSearch.UseVisualStyleBackColor = true;
             // 
             // buttonLoan
             // 
-            buttonLoan.Location = new Point(22, 244);
-            buttonLoan.Margin = new Padding(3, 2, 3, 2);
+            buttonLoan.Location = new Point(25, 325);
             buttonLoan.Name = "buttonLoan";
-            buttonLoan.Size = new Size(82, 22);
+            buttonLoan.Size = new Size(94, 29);
             buttonLoan.TabIndex = 11;
             buttonLoan.Text = "Loan";
             buttonLoan.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            button2.Location = new Point(687, 88);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 12;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // mainpage
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(853, 471);
+            ClientSize = new Size(975, 628);
+            Controls.Add(button2);
             Controls.Add(buttonLoan);
             Controls.Add(buttonSearch);
             Controls.Add(button1);
             Controls.Add(label1);
             Controls.Add(textBox1);
             Controls.Add(listBox1);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "mainpage";
             Text = "Form1";
             Load += mainpage_Load;
@@ -127,5 +130,6 @@
         private Button button1;
         private Button buttonSearch;
         private Button buttonLoan;
+        private Button button2;
     }
 }
